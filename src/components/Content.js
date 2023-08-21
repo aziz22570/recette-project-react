56447300485567df100487a358bb8f494e4cf31e
aziz <aziz2smaiii@gmail.com>
2023-08-21 15:33:55 +0100

    mise en place du contenu

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import styles from "./Content.module.scss";
 
 const Content = () => {
   return (
-    <div className='flex-fill'>Content</div>
-  )
-}
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={styles.grid}>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+        <div className={styles.elem}>element</div>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
 
-export default Content
+export default Content;
