7a5b436f11f253e8fdca70c8c3da2333dab99b27
aziz <aziz2smaiii@gmail.com>
2023-08-21 08:34:34 +0100

    mise en page de l'application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import styles from "./App.module.scss"
 function App() {
   return (
-    <h1>recette</h1>
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+    <Header />
+    <Content />
+    <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='b3 flex-fill'>Content</div>
+  )
+}
+
+export default Content
